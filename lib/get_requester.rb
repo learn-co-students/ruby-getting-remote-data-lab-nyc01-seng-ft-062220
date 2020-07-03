a17fcacdafd2c7c117bc0318c86c6225fc857dc8
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-03 16:24:21 -0400

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
-# Write your code here
+# Practice sending GET requests using Ruby to access (Fake) remote data
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+
+    attr_reader :uri
+
+    def initialize(url)
+        @uri = URI.parse(url)
+    end
+
+    def uri=(url)
+        @uri = URI.parse(url)
+    end
+
+    def get_response_body
+        response_body = Net::HTTP.get_response(uri).body
+    end
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+
+end
